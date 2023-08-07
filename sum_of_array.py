45adbcc29dbdd2226a820465a9216407b8dec8de
ezemiller <onejemechibueze@gmail.com>
2023-08-07 09:44:58 +0100

    summing all elements of arrays or List

---
+++ sum_of_array.py
@@ -0,0 +1,12 @@
+def sum_simple_array(array):
+    """
+    take an array/List ,sum all the element of the array
+    eg a =[1,2,3,4,5] = 1+2+3+4+5 =15
+    """
+
+    return sum(array)
+
+
+array_input =[5,4,6,7,8,9]
+func = sum_simple_array(array_input)
+print(func)
